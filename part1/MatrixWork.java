1d68fe1ad9828420fb56490fc0ce7d1df447a70b
Brent Lee <brenthlee@gmail.com>
2018-10-08 10:01:46 -0700

    started main

--- part1/MatrixWork.java
+++ part1/MatrixWork.java
@@ -5,3 +5,30 @@
  * Project2
  */
 
+import java.util.*;
+import java.io.File;
+
+public class MatrixWork {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inFile;
+        System.out.println("Enter a filename:");
+        inFile = scanner.nextLine();
+        File file = new File(inFile);
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (scanner.hasNextLine()) {
+        }
+    }
+
+    public static int[][] matrixProduct(int[][] A, int[][] B) {
+        if (A[0].length != B.length) {
+            throw new IllegalArgumentException("columns in A != rows in B");
+        }
+    }
+
+}
